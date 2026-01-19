df940d859e544a8bd7a9aacbb3cb2d5960f71cb2
Carlos Martinez <carlos@cagnazzo.uy>
2026-01-19 02:31:08 -0300

    emacs

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -18,3 +18,28 @@
         (left . 50)   ; Position it differently
         (top . 50)))
 
+(global-display-line-numbers-mode)
+
+(add-to-list 'default-frame-alist '(font . "Monaco-14"))
+(setq inhibit-startup-screen t)
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(tsdh-dark))
+ '(package-selected-packages '(markdown-mode zig-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
